cd61282a6cbcfd37178a300929c3dfdb4c5b20c3
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-06 02:25:27 +0300

    Added 'TrashOutlined' icon

--- src/shared/assets/icons/icons.tsx
+++ src/shared/assets/icons/icons.tsx
@@ -34,6 +34,23 @@ export const Icons = {
       </path>
     </svg>
   )),
+  TrashOutlined: svgWrapper((props, ref) => (
+    <svg
+      fill={'currentColor'}
+      height={24}
+      ref={ref}
+      viewBox={'0 0 24 24'}
+      width={24}
+      xmlns={'http://www.w3.org/2000/svg'}
+      {...props}
+    >
+      <path
+        d={
+          'M7 21q-.825 0-1.413-.588T5 19V6q-.425 0-.713-.288T4 5q0-.425.288-.713T5 4h4q0-.425.288-.713T10 3h4q.425 0 .713.288T15 4h4q.425 0 .713.288T20 5q0 .425-.288.713T19 6v13q0 .825-.588 1.413T17 21H7ZM17 6H7v13h10V6ZM9 17h2V8H9v9Zm4 0h2V8h-2v9ZM7 6v13V6Z'
+        }
+      ></path>
+    </svg>
+  )),
 }
 
 // ==============================================================================
