d20ba76079e026b5b670abf64237836e3dad6294
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-06 14:41:58 +0300

    Added 'Edit' icon

--- src/shared/assets/icons/icons.tsx
+++ src/shared/assets/icons/icons.tsx
@@ -5,6 +5,23 @@ import { withMemoizedForwardRef } from '@/shared/lib/HOC'
 const svgWrapper = withMemoizedForwardRef<SVGSVGElement, SVGProps<SVGSVGElement>>
 
 export const Icons = {
+  Edit: svgWrapper((props, ref) => (
+    <svg
+      fill={'currentColor'}
+      height={'1em'}
+      ref={ref}
+      viewBox={'0 0 24 24'}
+      width={'1em'}
+      xmlns={'http://www.w3.org/2000/svg'}
+      {...props}
+    >
+      <path
+        d={
+          'M5 19h1.4l8.625-8.625l-1.4-1.4L5 17.6V19ZM19.3 8.925l-4.25-4.2l1.4-1.4q.575-.575 1.413-.575t1.412.575l1.4 1.4q.575.575.6 1.388t-.55 1.387L19.3 8.925ZM17.85 10.4L7.25 21H3v-4.25l10.6-10.6l4.25 4.25Zm-3.525-.725l-.7-.7l1.4 1.4l-.7-.7Z'
+        }
+      ></path>
+    </svg>
+  )),
   Spinner: svgWrapper((props, ref) => (
     <svg
       fill={'currentColor'}
