611c14934178b1813873bddc76219ac1a94944b7
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-07 21:54:04 +0300

    Added 'loadBoards' for PromiseAll

--- src/app/app-loader.tsx
+++ src/app/app-loader.tsx
@@ -1,5 +1,6 @@
 import { type FC, type PropsWithChildren, useEffect, useState } from 'react'
 
+import { useBoards } from '@/entities/board'
 import { useSession } from '@/entities/session'
 import { useUsers } from '@/entities/user'
 import { PageSpinner } from '@/shared/ui'
@@ -7,15 +8,16 @@ import { PageSpinner } from '@/shared/ui'
 export const AppLoader: FC<PropsWithChildren> = ({ children }) => {
   const loadUsers = useUsers(state => state.loadUsers)
   const loadSession = useSession(state => state.loadSession)
+  const loadBoards = useBoards(state => state.loadBoards)
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     setIsLoading(true)
 
-    Promise.all([loadSession(), loadUsers()]).finally(() => {
+    Promise.all([loadSession(), loadUsers(), loadBoards()]).finally(() => {
       setIsLoading(false)
     })
-  }, [loadSession, loadUsers])
+  }, [loadSession, loadUsers, loadBoards])
 
   if (isLoading) {
     return <PageSpinner />
