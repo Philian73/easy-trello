10c0b554cb6b9f674fdb40f0df95aa2c88097192
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-10 01:39:37 +0300

    Added 'TextArea' ui in 'TextField.TextArea'

--- src/shared/ui/text-field/text-field.tsx
+++ src/shared/ui/text-field/text-field.tsx
@@ -1,24 +1,31 @@
-import { type ComponentProps, type ComponentPropsWithoutRef, forwardRef, useId } from 'react'
+import {
+  type ComponentProps,
+  type ComponentPropsWithoutRef,
+  type ForwardRefExoticComponent,
+  type RefAttributes,
+  forwardRef,
+  useId,
+} from 'react'
 
 import clsx from 'clsx'
 
 type TextFieldProps = {
   errorMessage?: string
+  fullWidth?: boolean
+  label?: string
   /**
    * Additional props for customizing the underlying `div` element,
    * which serves as the parent for the input.
    * Use it for setting `className` or `style`.
    */
-  fullWidth?: boolean
-  label?: string
   rootProps?: ComponentProps<'div'>
 } & ComponentPropsWithoutRef<'input'>
 
-export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+export const TextField = forwardRef(
   ({ className, errorMessage, fullWidth, id, label, rootProps, ...rest }, ref) => {
     const generatedId = useId()
 
-    const inputId = (id ?? generatedId) + 'input'
+    const inputId = id ?? generatedId + 'input'
 
     const showError = !!errorMessage && errorMessage?.length > 0
 
@@ -44,12 +51,64 @@ export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
       </div>
     )
   }
+) as ForwardRefExoticComponent<TextFieldProps & RefAttributes<HTMLInputElement>> & {
+  TextArea: typeof TextArea
+}
+
+// ==============================================================================
+
+type TextAreaProps = {
+  errorMessage?: string
+  fullWidth?: boolean
+  label?: string
+  /**
+   * Additional props for customizing the underlying `div` element,
+   * which serves as the parent for the input.
+   * Use it for setting `className` or `style`.
+   */
+  rootProps?: ComponentProps<'div'>
+} & ComponentPropsWithoutRef<'textarea'>
+
+const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
+  ({ className, errorMessage, fullWidth, id, label, rootProps, ...rest }, ref) => {
+    const generatedId = useId()
+    const textAreaId = id ?? generatedId + 'textarea'
+
+    const showError = !!errorMessage && errorMessage?.length > 0
+
+    return (
+      <div
+        {...rootProps}
+        className={clsx('flex flex-col gap-1', fullWidth && 'grow', rootProps?.className)}
+      >
+        {label && <label htmlFor={textAreaId}>{label}</label>}
+
+        <textarea
+          aria-invalid={showError}
+          className={clsx(
+            'rounded border border-slate-300 focus:border-teal-600 px-2 outline-none aria-[invalid=true]:border-rose-600 transition-all duration-200',
+            className
+          )}
+          id={textAreaId}
+          ref={ref}
+          {...rest}
+        />
+
+        {showError && <span className={'text-rose-400 text-sm'}>{errorMessage}</span>}
+      </div>
+    )
+  }
 )
+
+// ==============================================================================
+
+TextField.TextArea = TextArea
 
 // ==============================================================================
 
 if (import.meta.env.DEV) {
   TextField.displayName = 'TextField'
+  TextArea.displayName = 'TextField.TextArea'
 }
 
 // ==============================================================================
