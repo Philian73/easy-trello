51446875c53f24681ab74a4e688002e6c348ec9c
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-09 16:56:19 +0300

    changed max-width

--- src/shared/ui/layouts/center-content-layout/center-content-layout.tsx
+++ src/shared/ui/layouts/center-content-layout/center-content-layout.tsx
@@ -6,6 +6,6 @@ type CenterContentLayoutProps = ComponentPropsWithoutRef<'div'>
 
 export const CenterContentLayout: FC<CenterContentLayoutProps> = ({ className, ...rest }) => {
   return (
-    <div className={clsx('container mx-auto max-w-[600px] m-full px-10', className)} {...rest} />
+    <div className={clsx('container mx-auto max-w-[1000px] m-full px-10', className)} {...rest} />
   )
 }
