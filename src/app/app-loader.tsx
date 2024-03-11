515800b078471767b7d5b0c53abd494d2f2842d0
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-11 15:42:21 +0300

    used 'loadTasks' from useTasks

--- src/app/app-loader.tsx
+++ src/app/app-loader.tsx
@@ -2,6 +2,7 @@ import { type FC, type PropsWithChildren, useEffect, useState } from 'react'
 
 import { useBoards } from '@/entities/board'
 import { useSession } from '@/entities/session'
+import { useTasks } from '@/entities/task'
 import { useUsers } from '@/entities/user'
 import { PageSpinner } from '@/shared/ui'
 
@@ -9,15 +10,17 @@ export const AppLoader: FC<PropsWithChildren> = ({ children }) => {
   const loadUsers = useUsers(state => state.loadUsers)
   const loadSession = useSession(state => state.loadSession)
   const loadBoards = useBoards(state => state.loadBoards)
+  const loadTasks = useTasks(state => state.loadTasks)
+
   const [isLoading, setIsLoading] = useState(true)
 
   useEffect(() => {
     setIsLoading(true)
 
-    Promise.all([loadSession(), loadUsers(), loadBoards()]).finally(() => {
+    Promise.all([loadSession(), loadUsers(), loadBoards(), loadTasks()]).finally(() => {
       setIsLoading(false)
     })
-  }, [loadSession, loadUsers, loadBoards])
+  }, [loadSession, loadUsers, loadBoards, loadTasks])
 
   if (isLoading) {
     return <PageSpinner />
