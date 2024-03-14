3a4ac701579c4a079640d93d7e569403b7d23322
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-14 04:32:34 +0300

    changed types, added new types

--- src/entities/board/index.ts
+++ src/entities/board/index.ts
@@ -1,9 +1,14 @@
+export { boardsRepository } from './model/boards.repository'
 export { type BoardsStore, useBoards } from './model/boards.store'
 export type {
   Board,
   BoardCard,
-  BoardCol,
   BoardPartial,
+  BoardTask,
+  CreateBoardCardData,
   CreateBoardData,
+  CreateBoardTaskData,
+  UpdateBoardCardData,
   UpdateBoardData,
+  UpdateBoardTaskData,
 } from './model/types'
--- src/entities/board/model/types.ts
+++ src/entities/board/model/types.ts
@@ -1,37 +1,43 @@
-export type BoardPartial = {
-  editorsIds: string[]
-  id: string
-  ownerId: string
-  title: string
-}
-
 export type Board = {
-  cols: BoardCol[]
+  authorId: string
+  cards: BoardCard[]
+  created: string
   editorsIds: string[]
   id: string
   ownerId: string
   title: string
-}
-
-export type BoardCol = {
-  id: string
-  items: BoardCard[]
-  title: string
+  updated: string
 }
 
 export type BoardCard = {
+  authorId: string
+  created: string
+  id: string
+  tasks: BoardTask[]
+  title: string
+  updated: string
+}
+
+export type BoardTask = {
   assigneeId?: string
+  authorId: string
+  created: string
+  description?: string
   id: string
   title: string
+  updated: string
 }
 
-export type CreateBoardData = {
-  editorsIds: string[]
-  title: string
-}
+export type BoardPartial = Omit<Board, 'authorId' | 'cards'>
 
-export type UpdateBoardData = {
-  editorsIds?: string[]
-  ownerId?: string
-  title?: string
-}
+export type CreateBoardData = Pick<Board, 'editorsIds' | 'title'>
+
+export type UpdateBoardData = Partial<Pick<Board, 'editorsIds' | 'ownerId' | 'title'>>
+
+export type CreateBoardCardData = Pick<BoardCard, 'title'>
+
+export type UpdateBoardCardData = Pick<BoardCard, 'title'>
+
+export type CreateBoardTaskData = Pick<BoardTask, 'title'>
+
+export type UpdateBoardTaskData = Partial<Pick<BoardTask, 'assigneeId' | 'description' | 'title'>>
