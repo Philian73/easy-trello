cb42dff6c81161df58b8955083d1125bb17e7c63
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-15 16:28:43 +0300

    ToastContainer

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,26 @@
+import { Bounce, ToastContainer } from 'react-toastify'
+
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
+import 'react-toastify/dist/ReactToastify.css'
 
 import { App } from './app'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <>
+    <App />
+    <ToastContainer
+      autoClose={5000}
+      closeOnClick
+      draggable={false}
+      hideProgressBar={false}
+      newestOnTop
+      pauseOnFocusLoss={false}
+      pauseOnHover
+      position={'bottom-right'}
+      rtl={false}
+      transition={Bounce}
+    />
+  </>
+)
