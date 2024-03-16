6737edb6a23c5256546afb480f059d5535ff9b15
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-16 17:20:52 +0300

    added 'boundaries' plugin and its settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+  root: true,
   env: {
     amd: true,
     browser: true,
@@ -12,6 +13,7 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'prettier/prettier',
     'plugin:import/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:import/typescript',
     'plugin:prettier/recommended',
     'plugin:perfectionist/recommended-natural',
@@ -34,7 +36,7 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'import', 'perfectionist'],
+  plugins: ['react', '@typescript-eslint', 'import', 'perfectionist', 'boundaries'],
   rules: {
     'arrow-parens': 'off',
     'consistent-return': 'off',
@@ -101,6 +103,147 @@ module.exports = {
     'react/jsx-fragments': ['error'],
     'react/prop-types': 'off',
     'react/void-dom-elements-no-children': ['error'],
+    'react-refresh/only-export-components': 0,
+    'boundaries/entry-point': [
+       2,
+      {
+        default: 'disallow',
+        rules: [
+          {
+            target: [
+              [
+                'shared',
+                {
+                  segment: 'lib',
+                },
+              ],
+            ],
+            allow: '*/index.ts',
+          },
+          {
+            target: [
+              [
+                'shared',
+                {
+                  segment: 'lib',
+                },
+              ],
+            ],
+            allow: '*.(ts|tsx)',
+          },
+          {
+            target: [
+              [
+                'shared',
+                {
+                  segment: 'constants',
+                },
+              ],
+            ],
+            allow: 'index.ts',
+          },
+          {
+            target: [
+              [
+                'shared',
+                {
+                  segment: 'ui',
+                },
+              ],
+            ],
+            allow: 'index.ts',
+          },
+          {
+            target: ['app', 'pages', 'widgets', 'features', 'entities'],
+            allow: 'index.(ts|tsx)',
+          },
+        ],
+      },
+    ],
+    'boundaries/element-types': [
+       2,
+      {
+        default: 'allow',
+        message: '${file.type} is not allowed to import (${dependency.type})',
+        rules: [
+          {
+            from: ['shared'],
+            disallow: ['app', 'pages', 'widgets', 'features', 'entities'],
+            message:
+               'Shared module must not import upper layers (${dependency.type})',
+          },
+          {
+            from: ['entities'],
+            message: 'Entity must not import upper layers (${dependency.type})',
+            disallow: ['app', 'pages', 'widgets', 'features'],
+          },
+          {
+            from: ['entities'],
+            message: 'Entity must not import other entity',
+            disallow: [
+              [
+                'entities',
+                {
+                  entity: '!${entity}',
+                },
+              ],
+            ],
+          },
+          {
+            from: ['features'],
+            message: 'Feature must not import upper layers (${dependency.type})',
+            disallow: ['app', 'pages', 'widgets'],
+          },
+          {
+            from: ['features'],
+            message: 'Feature must not import other feature',
+            disallow: [
+              [
+                'features',
+                {
+                  feature: '!${feature}',
+                },
+              ],
+            ],
+          },
+          {
+            from: ['widgets'],
+            message:
+               'Feature must not import upper layers (${dependency.type})',
+            disallow: ['app', 'pages'],
+          },
+          {
+            from: ['widgets'],
+            message: 'Widget must not import other widget',
+            disallow: [
+              [
+                'widgets',
+                {
+                  widget: '!${widget}',
+                },
+              ],
+            ],
+          },
+          {
+            from: ['pages'],
+            message: 'Page must not import upper layers (${dependency.type})',
+            disallow: ['app'],
+          },
+          {
+            from: ['pages'],
+            message: 'Page must not import other page',
+            disallow: [
+              [
+                'pages',
+                {
+                  page: '!${page}',
+                },
+              ],
+            ],
+          },
+        ],
+      },
+    ],
   },
   settings: {
     'import/parsers': {
@@ -115,6 +258,38 @@ module.exports = {
         alwaysTryTypes: true,
       },
     },
+    'boundaries/include': ['src/**/*'],
+    'boundaries/elements': [
+      {
+        type: 'app',
+        pattern: 'app',
+      },
+      {
+        type: 'pages',
+        pattern: 'src/pages/*',
+        capture: ['page'],
+      },
+      {
+        type: 'widgets',
+        pattern: 'widgets/*',
+        capture: ['widget'],
+      },
+      {
+        type: 'features',
+        pattern: 'features/*',
+        capture: ['feature'],
+      },
+      {
+        type: 'entities',
+        pattern: 'entities/*',
+        capture: ['entity'],
+      },
+      {
+        type: 'shared',
+        pattern: 'shared/*',
+        capture: ['segment'],
+      },
+    ],
 
     react: {
       version: 'detect',
