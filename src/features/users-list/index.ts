579dd06958ebead6124d460766887cb074644903
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-24 19:27:17 +0300

    feat: deleted 'RoleSelect' export from public api

--- src/features/users-list/index.ts
+++ src/features/users-list/index.ts
@@ -1,3 +1,2 @@
 export { CreateUserForm } from './ui/create-user-form/create-user-form'
-export { RoleSelect } from './ui/role-select/role-select'
 export { UsersList } from './ui/users-list/users-list'
