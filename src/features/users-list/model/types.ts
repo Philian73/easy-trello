30ce5b256c4d5f5f4e91eccc349dbdb079e9e43e
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-24 19:09:53 +0300

    feat: added 'User', 'UserRole' types, changed 'CreateUserFormData' type

--- src/features/users-list/model/types.ts
+++ src/features/users-list/model/types.ts
@@ -1,4 +1,15 @@
-export type CreateUserFormData = {
+export type UserRole = 'admin' | 'user'
+
+export type User = {
   avatarId: string
+  created: string
+  email: string
+  id: string
   name: string
+  role: UserRole
+  updated: string
+}
+
+export type CreateUserFormData = Pick<User, 'avatarId' | 'email' | 'name' | 'role'> & {
+  password: string
 }
