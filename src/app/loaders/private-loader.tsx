7287018b28b1dca154cd1bf7893cc53ec8388671
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-26 16:16:29 +0300

    feat: added private loader (for auth users)

--- src/app/loaders/private-loader.tsx
+++ src/app/loaders/private-loader.tsx
@@ -1,12 +1,11 @@
 import { type FC, type PropsWithChildren, useEffect, useState } from 'react'
 
 import { boardsQuery } from '@/entities/board'
-import { sessionQuery } from '@/entities/session'
 import { usersQuery } from '@/entities/user'
 import { PageSpinner } from '@/shared/ui'
 import { useQueryClient } from '@tanstack/react-query'
 
-export const AppLoader: FC<PropsWithChildren> = ({ children }) => {
+export const PrivateLoader: FC<PropsWithChildren> = ({ children }) => {
   const queryClient = useQueryClient()
 
   const [isLoading, setIsLoading] = useState(true)
@@ -15,16 +14,15 @@ export const AppLoader: FC<PropsWithChildren> = ({ children }) => {
     setIsLoading(true)
 
     Promise.all([
-      queryClient.prefetchQuery(sessionQuery),
-      queryClient.prefetchQuery(usersQuery),
       queryClient.prefetchQuery(boardsQuery),
+      queryClient.prefetchQuery(usersQuery),
     ]).finally(() => {
       setIsLoading(false)
     })
   }, [queryClient])
 
   if (isLoading) {
-    return <PageSpinner />
+    return <PageSpinner isLoading={isLoading} />
   }
 
   return <>{children}</>
