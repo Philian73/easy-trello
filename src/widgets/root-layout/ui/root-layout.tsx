3dc5718bc6b102a1c95a6b0771b23acd5e5c4ddb
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-26 17:44:51 +0300

    feat: added SignOutButton for header (auth)

--- src/widgets/root-layout/ui/root-layout.tsx
+++ src/widgets/root-layout/ui/root-layout.tsx
@@ -3,6 +3,7 @@ import type { AuthOutletContext } from '@/widgets/root-layout'
 import { Outlet } from 'react-router-dom'
 
 import { sessionQuery } from '@/entities/session'
+import { SignOutButton } from '@/features/auth'
 import { Header, PageSpinner } from '@/shared/ui'
 import { useQuery } from '@tanstack/react-query'
 
@@ -18,11 +19,18 @@ export const RootLayout = () => {
     return <PageSpinner isLoading={isLoading} />
   }
 
+  const renderAuthContentSlot = (
+    <div className={'flex gap-4 items-center ml-auto'}>
+      <Profile />
+      <SignOutButton />
+    </div>
+  )
+
   return (
     <div className={'min-h-screen flex flex-col'}>
       <Header
         linksContentSlot={isAuthenticated && <NavLinks />}
-        rightContentSlot={isAuthenticated && <Profile />}
+        rightContentSlot={isAuthenticated && renderAuthContentSlot}
       />
       <main className={'grow flex flex-col'}>
         <Outlet context={{ isAuthenticated } satisfies AuthOutletContext} />
