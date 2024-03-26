300d5f24fd4a58c0939054248d3347c8d35d1bfb
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-26 16:16:57 +0300

    feat: time display

--- src/shared/ui/page-spinner/page-spinner.tsx
+++ src/shared/ui/page-spinner/page-spinner.tsx
@@ -1,12 +1,24 @@
 import { type ComponentPropsWithoutRef, forwardRef } from 'react'
 
 import { Icons } from '@/shared/assets/icons'
+import { useAppearanceDelay } from '@/shared/lib/hooks'
 import clsx from 'clsx'
 
-type PageSpinnerProps = Omit<ComponentPropsWithoutRef<'div'>, 'children'>
+type PageSpinnerProps = {
+  isLoading?: boolean
+} & Omit<ComponentPropsWithoutRef<'div'>, 'children'>
 
 export const PageSpinner = forwardRef<HTMLDivElement, PageSpinnerProps>(
-  ({ className, ...rest }, ref) => {
+  ({ className, isLoading, ...rest }, ref) => {
+    const isShown = useAppearanceDelay(isLoading, {
+      defaultValue: true,
+      minDisplay: 500,
+    })
+
+    if (!isShown) {
+      return null
+    }
+
     return (
       <div
         className={clsx(
