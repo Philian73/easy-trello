afcd8a049172972ab14f15f6b3af9ff54ab641d8
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-27 19:12:28 +0300

    feat: isInitialized condition

--- src/shared/lib/i18n.ts
+++ src/shared/lib/i18n.ts
@@ -4,22 +4,24 @@ import i18n from 'i18next'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import Backend from 'i18next-http-backend'
 
-i18n
-  .use(Backend)
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    backend: {
-      loadPath: '/locales/{{lng}}/translation.json',
-    },
-    debug: import.meta.env.DEV,
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-    react: {
-      useSuspense: true,
-    },
-  })
+if (!i18n.isInitialized) {
+  i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+      backend: {
+        loadPath: '/locales/{{lng}}/translation.json',
+      },
+      debug: import.meta.env.DEV,
+      fallbackLng: 'en',
+      interpolation: {
+        escapeValue: false,
+      },
+      react: {
+        useSuspense: true,
+      },
+    })
+}
 
 export default i18n
