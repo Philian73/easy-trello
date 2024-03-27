dc9f03ac4e7cc87c6206c1872495171ff65ee676
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-27 05:08:43 +0300

    feat: remove ToastContainer

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,5 +1,3 @@
-import { Bounce, ToastContainer } from 'react-toastify'
-
 import { createRoot } from 'react-dom/client'
 
 import './index.css'
@@ -11,21 +9,5 @@ import { App } from './app'
 import('@/shared/api/msw')
   .then(({ worker }) => worker.start())
   .then(() => {
-    createRoot(document.getElementById('root')!).render(
-      <>
-        <App />
-        <ToastContainer
-          autoClose={5000}
-          closeOnClick
-          draggable={false}
-          hideProgressBar={false}
-          newestOnTop
-          pauseOnFocusLoss={false}
-          pauseOnHover
-          position={'bottom-right'}
-          rtl={false}
-          transition={Bounce}
-        />
-      </>
-    )
+    createRoot(document.getElementById('root')!).render(<App />)
   })
