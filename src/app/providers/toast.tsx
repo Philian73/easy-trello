fb833b90ce1eea5c6dc9ccbd27b930857d805bb1
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-27 05:08:32 +0300

    feat: Created ToastProvider

---
+++ src/app/providers/toast.tsx
@@ -0,0 +1,27 @@
+import type { FC, PropsWithChildren } from 'react'
+import { Bounce, ToastContainer } from 'react-toastify'
+
+import { useTheme } from '@/shared/lib/theme'
+
+export const Toast: FC<PropsWithChildren> = ({ children }) => {
+  const { theme } = useTheme()
+
+  return (
+    <>
+      {children}
+      <ToastContainer
+        autoClose={5000}
+        closeOnClick
+        draggable={false}
+        hideProgressBar={false}
+        newestOnTop
+        pauseOnFocusLoss={false}
+        pauseOnHover
+        position={'bottom-right'}
+        rtl={false}
+        theme={theme}
+        transition={Bounce}
+      />
+    </>
+  )
+}
