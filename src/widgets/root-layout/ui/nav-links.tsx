6d17f27a50fa01df55a345e0c8d6b3daa8c7d516
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2024-03-27 20:13:55 +0300

    feat: translate NavLinks in header

--- src/widgets/root-layout/ui/nav-links.tsx
+++ src/widgets/root-layout/ui/nav-links.tsx
@@ -1,18 +1,21 @@
+import { useTranslation } from 'react-i18next'
 import { NavLink, type NavLinkProps } from 'react-router-dom'
 
 import { ROUTER_PATHS } from '@/shared/constants'
 import clsx from 'clsx'
 
 export const NavLinks = () => {
+  const { t } = useTranslation()
+
   const linkClasses: NavLinkProps['className'] = ({ isActive }) => clsx(isActive && 'underline')
 
   return (
     <div className={'text-lg flex gap-5'}>
       <NavLink className={linkClasses} to={ROUTER_PATHS.USERS}>
-        Пользователи
+        {t('pages.users.title.users')}
       </NavLink>
       <NavLink className={linkClasses} to={ROUTER_PATHS.BOARDS}>
-        Доски
+        {t('pages.boards.title.boards')}
       </NavLink>
     </div>
   )
